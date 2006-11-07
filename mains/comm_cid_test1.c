76029ee07996e6b3fefa2a37b260298dbe0dc4a7
mchaarawi <mchaarawi@gmail.com>
2006-11-07 23:24:22 +0000

    added duplication cases for MPI_COMM_SELF

--- mains/comm_cid_test1.c
+++ mains/comm_cid_test1.c
@@ -9,7 +9,7 @@ int main( int argc, char *argv[] )
   struct timeval tv,tv1;
   struct timezone tz,tz1;
   /*struct tm *tm,*tm1;*/
-  MPI_Comm comm;
+  MPI_Comm comm,comm_self,dup1,dup2;
   int rank, size, rank2, size2, i , n; 
   MPI_Comm *commList; 
 
@@ -20,11 +20,20 @@ int main( int argc, char *argv[] )
 
   MPI_Init ( &argc, &argv );
   comm = MPI_COMM_WORLD;
+  comm_self = MPI_COMM_SELF;
   n = 2000;
   i = 0;
 
   MPI_Comm_rank ( comm, &rank );
   MPI_Comm_size ( comm, &size );
+
+  MPI_Comm_dup ( comm_self, &dup1 );
+  MPI_Comm_dup ( comm_self, &dup2 );
+
+  if(rank % 2 == 0) 
+      MPI_Comm_free(&dup1);
+  else 
+      MPI_Comm_free(&dup2);
 
   commList = (MPI_Comm *)malloc(n * (sizeof(MPI_Comm)) );
 
@@ -40,8 +49,10 @@ int main( int argc, char *argv[] )
 
   printf("Execution time = %d sec %d microseconds \n",
 	 //(tm1->tm_hour - tm->tm_hour), (tm1->tm_min - tm->tm_min),
-	 (tv1.tv_sec - tv.tv_sec),
-	 tv1.tv_usec > tv.tv_usec ? (tv1.tv_usec - tv.tv_usec) : 1000000 + (tv1.tv_usec - tv_usec) );
+	 tv1.tv_usec > tv.tv_usec ? (tv1.tv_sec - tv.tv_sec)
+	 : (tv1.tv_sec - tv.tv_sec - 1),
+	 tv1.tv_usec > tv.tv_usec ? (tv1.tv_usec - tv.tv_usec) 
+	 : 1000000 + (tv1.tv_usec - tv.tv_usec) );
 
   MPI_Finalize ();
 
